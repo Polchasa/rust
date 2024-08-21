f3df13e894730d4f2586da99849e00c42ec6ccc7
Polchasa <polchasa@live.ru>
2024-08-21 19:21:29 +0300

    Управление растущими проектами

---
+++ projects/restaurant/target/debug/deps/restaurant-6021b45614e8f3ef.d
@@ -0,0 +1,5 @@
+/Users/polchasa/Документы/rust/projects/restaurant/target/debug/deps/librestaurant-6021b45614e8f3ef.rmeta: src/lib.rs
+
+/Users/polchasa/Документы/rust/projects/restaurant/target/debug/deps/restaurant-6021b45614e8f3ef.d: src/lib.rs
+
+src/lib.rs:
---
+++ projects/restaurant/target/debug/deps/restaurant-76636bc2b04199a5.d
@@ -0,0 +1,5 @@
+/Users/polchasa/Документы/rust/projects/restaurant/target/debug/deps/librestaurant-76636bc2b04199a5.rmeta: src/lib.rs
+
+/Users/polchasa/Документы/rust/projects/restaurant/target/debug/deps/restaurant-76636bc2b04199a5.d: src/lib.rs
+
+src/lib.rs:
